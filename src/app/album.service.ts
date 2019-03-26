111bdb8086ffa50feb7dd67e677b522796ef2273
Manuel Sande <mnlsande22@gmail.com>
2019-03-26 04:27:23 -0400

    Create functions to deliver service to marketplace & album-detail components

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,9 +1,28 @@
+/* A service file with an injector responsible for delivering this service
+wherever it's needed in the project.ie, getAlbums() {return ALBUMS;}*/
+
 import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { ALBUMS } from './mock-albums';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AlbumService {
 
   constructor() { }
+
+  getAlbums(){
+    return ALBUMS;
+  }
+
+  getAlbumsById(albumId: number) {
+    for (var i = 0; i <= ALBUMS.length -1; i++) {
+      if (ALBUMS[i].id === albumId) {
+        return ALBUMS[i];
+      }
+    }
+  }
+  
 }
